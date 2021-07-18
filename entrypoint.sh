b88dea20de76bffd08cbfc85a1f59c36b5cf04f4
Acxym <63164394+Acxym@users.noreply.github.com>
2021-07-18 11:55:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,11 @@ DIR_TMP="$(mktemp -d)"
 curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/nondanee/UnblockNeteaseMusic/archive/refs/tags/v0.25.3.zip -o ${DIR_TMP}/UnblockNeteaseMusic.zip
 unzip ${DIR_TMP}/UnblockNeteaseMusic.zip -d ${DIR_TMP}
 
+#Get node.js
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL nodejs.org/dist/v12.18.1/node-v12.18.1-linux-x64.tar.xz -o ${DIR_TMP}/node.zip
+tar xf ${DIR_TMP}/node.tar.xz -d ${DIR_RUNTIME}
+export PATH=$PATH:${DIR_RUNTIME}/node-v12.18.1-linux-x64/bin
+
 # Install UnblockNeteaseMusic
 mv ${DIR_TMP}/UnblockNeteaseMusic-0.25.3 ${DIR_RUNTIME}/UnblockNeteaseMusic
 rm -rf ${DIR_TMP}
