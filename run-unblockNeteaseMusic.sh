80257f80e10bb13e1dcf7017220617fdb3267feb
Acxym <63164394+Acxym@users.noreply.github.com>
2021-07-18 22:26:25 +0800

    Delete run-unblockNeteaseMusic.sh

--- run-unblockNeteaseMusic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Global variables
-DIR_CONFIG="/etc/UnblockNeteaseMusic"
-DIR_RUNTIME="/usr/bin"
-
-# Run UnblockNeteaseMusic
-node ${DIR_RUNTIME}/UnblockNeteaseMusic/app.js -s -e http://music.163.com -p 8080
