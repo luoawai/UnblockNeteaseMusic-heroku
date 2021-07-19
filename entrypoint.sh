030b5d7407c1c5ab03dc499e4ad84ef088260a29
Acxym <63164394+Acxym@users.noreply.github.com>
2021-07-19 16:50:23 +0800

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -63,7 +63,8 @@ install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
 rm -rf ${DIR_TMP}
 
 # Run
-${DIR_RUNTIME}/v2ray --config=${DIR_CONFIG}/config.pb & node ${DIR_RUNTIME}/UnblockNeteaseMusic/app.js -s -e http://music.163.com -p 8080
+node ${DIR_RUNTIME}/UnblockNeteaseMusic/app.js -s -e http://music.163.com -p 8080 & ${DIR_RUNTIME}/v2ray --config=${DIR_CONFIG}/config.pb
+
 # Version
 node -v
 npx -v
