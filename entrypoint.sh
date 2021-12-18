f96f8b69c87e63b87cb5b1fb27636ffed4048875
luoawai <32483950+luoawai@users.noreply.github.com>
2021-12-18 19:20:54 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,65 @@ DIR_CONFIG="/etc/UnblockNeteaseMusic"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
+# Write V2Ray configuration
+cat << EOF > ${DIR_TMP}/heroku.json
+{
+    "inbounds": [{
+        "port": ${PORT},
+        "protocol": "vmess",
+        "settings": {
+            "clients": [{
+                "id": "${ID}",
+                "alterId": ${AID}
+            }]
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path": "${WSPATH}"
+            }
+        },
+		"proxySettings": {
+		"tag": "HTTP"
+        }
+    }],
+	"outbounds": [{
+		"protocol": "http",
+		"settings": {
+		"servers": [
+			{
+				"address": "127.0.0.1",
+				"port": 8080,
+				"users": []
+			}
+		]
+		},
+		"tag": "HTTP"
+    }]
+}
+EOF
+
+# Get V2Ray executable release
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
+busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
+
 # Get UnblockNeteaseMusic executable release
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/UnblockNeteaseMusic/server/archive/refs/tags/v0.27.0-rc.2.zip -o ${DIR_TMP}/UnblockNeteaseMusic.zip
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/UnblockNeteaseMusic/server/archive/refs/tags/v0.27.0-rc.2.zip-o ${DIR_TMP}/UnblockNeteaseMusic.zip
 busybox unzip ${DIR_TMP}/UnblockNeteaseMusic.zip -d ${DIR_TMP}
+
+# Convert to protobuf format configuration
+mkdir -p ${DIR_CONFIG}
+${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
 
 # Install UnblockNeteaseMusic
 mv ${DIR_TMP}/server-0.27.0-rc.2 ${DIR_RUNTIME}/UnblockNeteaseMusic
 
+# Install V2Ray
+install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
+rm -rf ${DIR_TMP}
+
 # Run
-node ${DIR_RUNTIME}/UnblockNeteaseMusic/app.js -p 8080 ${UnblockNeteaseMusicStartup}
+node ${DIR_RUNTIME}/UnblockNeteaseMusic/app.js -p 8080 ${UnblockNeteaseMusicStartup} & ${DIR_RUNTIME}/v2ray --config=${DIR_CONFIG}/config.pb
 
 # Version
 node -v
